7f6576e3a6a08fb808fe213130f78a91b12bd8fc
AmeenFlow <ameenflow72@gmail.com>
2026-05-23 13:01:07 +0300

    Initial project setup

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+
+
+// الاعدادات العامة للموقع
+export const siteConfig = {
+    name: "ِHybrid Cars",
+    description: "A modern bilingual SaaS dashboard for teams.",
+    url: "http://localhost:3000",
+    locales: ["ar", "en"], //اللغات المدعومة
+    defaultLocale: "ar",// اللغة الافتراضية
+} as const;//للتعامل مع القم كثوابت دقيقة و ليس كنصوص عامة فقط
+
+// ==============================================================
